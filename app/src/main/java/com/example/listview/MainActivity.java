ebb76ad9739b3ddfea01264ea60165f0e84d38fc
ofekgani <ag8339@bs.amalnet.k12.il>
2019-09-14 18:40:50 +0300

    no message

--- app/src/main/java/com/example/listview/MainActivity.java
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.listview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     ListView listView;
     Button button;
     String[] Continents = {"America", "Europe", "Asia", "Africa"};
+
+    Intent si = new Intent(this,MainActivity2.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +26,23 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         button = findViewById(R.id.btn_next);
 
+        listView.setOnItemClickListener(this);
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
         ArrayAdapter<String> adp = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item,Continents);
         listView.setAdapter(adp);
+
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast toast = Toast.makeText(this,""+Continents[i],Toast.LENGTH_SHORT);
+        toast.show();
+        //si.putExtra("n",Continents[i]);
+    }
+
+    public void next(View view) {
+        //startActivity(si);
     }
 }
